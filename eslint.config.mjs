b41a591062aff4b0dcbb48be203b129867290bd6
vladkutnyakov <kutnyakov1@yandex.ru>
2025-04-08 22:36:27 +0400

    eslint, prettier fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   {
@@ -24,12 +25,17 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      '@stylistic': stylistic,
+    },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'semi': ['error', 'never'],
+      '@stylistic/indent': ['error', 2],
     },
   },
 );
