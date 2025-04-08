5740aa2577009a71dd899d67d16bd88f19c02316
vladkutnyakov <kutnyakov1@yandex.ru>
2025-04-08 23:55:49 +0400

    ai module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AiModule } from './ai/ai.module'
+import { AiController } from './ai/ai.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AiModule],
+  controllers: [AppController, AiController],
   providers: [AppService],
 })
 export class AppModule {}
